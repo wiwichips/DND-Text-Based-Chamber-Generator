ae9fb6f0675926d77912c79fb64d8dc52c237e56
Will <wpringle@uoguelph.ca>
2019-09-13 21:36:29 -0400

    adding the treasure to the main file

--- src/dnd/dungeon.java
+++ src/dnd/dungeon.java
@@ -20,18 +20,33 @@ public class dungeon{
 		// Generate the Shape of the room
 		chamberShape.setShape(d20.roll());
 		ArrayList list = chamberShape.getExits();
-		// Print the shape, size and 
+		
+		// Print Aspects of the chamber 
 		System.out.print(chamberShape.getShape());
 		try{
 			System.out.print(" " + chamberShape.getWidth());
 		}
-		
-		catch(UnusualShapeException e){
-			
+		catch(UnusualShapeException e){	
 		}
 		
 		System.out.println("Exits: " + list.size());
 		
+		
+		// Generate the treasure
+		Treasure roomTreasure = new Treasure();
+		roomTreasure.setDescription();
+		roomTreasure.setContainer(d20.roll());
+		
+		System.out.println("In terms of treasure, there's " + roomTreasure.getDescription() + " inside of " + roomTreasure.getContainer());
+		
+		try{
+			System.out.println("The treasure is protected by " + roomTreasure.getProtection()); // FIX, SOMTIMES NULL???
+		}
+		catch(NotProtectedException e){
+			System.out.println("No one is watching over the treasure.");
+		}
+
+		// Generate the 
 	}
 
 
